55019b11c204433ae910628eae03b431c0b3c471
Sean Hopkins <seahopki12@gmail.com>
2020-09-21 21:38:28 -0400

    Created js file for weather API and added script tag to HTML file

---
+++ weatherAPI.js
@@ -0,0 +1,8 @@
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=hampton&appid=3e0e5c5529a43577f74e6903ca5baa30";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+});
